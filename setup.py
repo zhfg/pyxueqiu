6b2200abaccc8c9cb4db0bf0c020cf297f4ba09c
jacob <jacob.zhang@lianwei.com.cn>
2021-12-11 16:43:07 +0800

    fix

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": ""},
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
